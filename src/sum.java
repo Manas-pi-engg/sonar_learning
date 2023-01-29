8797f9ccb05ef0272560a1ead1ef5bce84d1d271
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-01-29 23:07:38 +0530

    Update sum.java

--- src/sum.java
+++ src/sum.java
@@ -3,8 +3,7 @@ public class sum {
     {
         int a=5,b=7;
 int d=0;
-        i cd=0;
         System.out.println("hello world sum");
-        System.out.println(a+b+d +cd);
+        System.out.println(a+b+d);
     }
 }
