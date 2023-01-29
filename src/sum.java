451be47d8351796c712eeae3620bb4804d27163b
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-01-29 22:48:12 +0530

    Create sum.java

--- src/sum.java
+++ src/sum.java
@@ -2,6 +2,7 @@ public class sum {
     public static void main(String[] args)
     {
         int a=5,b=7;
+int d=0;
         System.out.println("hello world sum");
         System.out.println(a+b);
     }
